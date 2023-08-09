bbc4000974e399e745cbe76e149c86f25b814166
LuisLuna24 <eduarlun4@gmail.com>
2023-08-09 11:32:33 -0600

    Empese Ventana

--- Principal.php
+++ Principal.php
@@ -18,17 +18,7 @@ if(!isset($correo)){
         Cabezera
     ==================================================================================-->
     <header class="header">
-        <a href="#" class="logo">AN</a>
-        <input type="checkbox" id="side_menu" class="side_menu">
-        <label for="side_menu" class="hamb"><span class="hamb_line"></span></label>
-        <nav class="nav">
-            <ul class="menu">
-                <li><a href="#">Inventario</a></li>
-                <li><a href="#">Empeleados</a></li>
-                <li><a href="#">Venta</a></li>
-                <li><a href="php/salir.php">Salir</a></li>
-            </ul>
-        </nav>
+        <?php require('Global/cabesera.php'); ?> 
     </header>
     <!--=================================================================================
         Primera parte
--- Venta.php
+++ Venta.php
@@ -11,9 +11,29 @@ if(!isset($correo)){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ventas</title>
+    <link rel="stylesheet" href="css/Venta.css">
 </head>
 <body>
-    <h1>Hola soy Ventas</h1>
+    <header class="header">
+        <?php require('Global/cabesera.php'); ?> 
+    </header>
+    <!--=================================================================================
+        Primera parte
+    ==================================================================================-->
+    <section class="Ventas_Contenedor">
+        <Section class="Vetas_Formularios">
+            <form action="" method="post" class="Ventas_Busqueda">
+                <div class="Venta_Campo">
+                    <input class="Venta_Doc" type="text" required name="contraseña" id="contraseña">
+                    <label for="text">Contraseña</label>
+                </div>
+                
+                <input type="submit" value="">
+            </form>
+        </Section>
+    </section>
+
+
 </body>
 </html>
 
